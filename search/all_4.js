9ca5961b330bf0db1fbbda5fc570aed10e1fad66
System-rat <40741859+System-rat@users.noreply.github.com>
2022-04-14 11:59:48 +0000

    Deploying to gh-pages from @ System-rat/ansi@be34e7f873ec3da470298d8a4fd5380443cd4ec9 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['manipulatorfunc_9',['ManipulatorFunc',['../classansi_1_1ManipulatorFunc.html',1,'ansi::ManipulatorFunc'],['../classansi_1_1ManipulatorFunc.html#a7ecd3b62036f13beb1656c5ba6660e95',1,'ansi::ManipulatorFunc::ManipulatorFunc()']]],
   ['modifier_10',['modifier',['../namespaceansi_1_1manip.html#ab0e752a07f55e0c052a65bc1f01ca875',1,'ansi::manip']]],
-  ['move_5fcursor_11',['move_cursor',['../namespaceansi_1_1manip.html#a11bbd865092de541d140a8b79340a3a7',1,'ansi::manip::move_cursor(const CursorDirection direction, const uint amount)'],['../namespaceansi_1_1manip.html#aa25330e1d49d7d91d9895d4d487741d3',1,'ansi::manip::move_cursor(const uint x, const uint y)']]],
+  ['move_5fcursor_11',['move_cursor',['../namespaceansi_1_1manip.html#a306ae03457b1e46710ffe90b947f579f',1,'ansi::manip::move_cursor(const CursorDirection direction, const uint8_t amount)'],['../namespaceansi_1_1manip.html#a4f8b2c86efbe8bf125d3f03517d7e9d3',1,'ansi::manip::move_cursor(const uint8_t x, const uint8_t y)']]],
   ['move_5fcursor_5fhome_12',['move_cursor_home',['../namespaceansi_1_1manip.html#a10379217a36596b164568e8f1471326f',1,'ansi::manip']]]
 ];
